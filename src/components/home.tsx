9f230bc2f9b6b993b3331a71347cd7a8b49a8c62
jkarathra <josh.karathra@speechdojo.org>
2024-12-02 17:35:47 +0000

    Hello world

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,8 +1,9 @@
 function Home() {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-screen flex items-center justify-center bg-background">
+      <h1 className="text-4xl font-bold">Hello World!</h1>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
